740a31ef25c94ce6e22cff1754b634c7392d8fa5
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-06-21 13:58:50 -0600

    Likes updating

--- src/App/App.js
+++ src/App/App.js
@@ -15,24 +15,53 @@ class App extends Component {
   }
 
   fetchBeers() {
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    let targetUrl = 'http://beer.fluentcloud.com/v1/beer';
-    fetch(proxyUrl + targetUrl)
+    fetch('http://beer.fluentcloud.com/v1/beer')
     .then(response => response.json())
     .then(data => this.setState({ beer: data }))
     .catch(error => console.error(error))
   }
 
+  updateBeerLikes(id, likes) {
+    fetch(`http://beer.fluentcloud.com/v1/beer/${id}`, {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({ likes })
+    })
+    .then(response => this.fetchBeers())
+    .catch(error => console.error(error))
+  }
+
+  postNewBeer(name, likes) {
+    fetch('http://beer.fluentcloud.com/v1/beer', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({ name, likes })
+    })
+    .then(response => this.fetchBeers())
+    .catch(error => console.error(error))
+  }
+
+  updateLikes(e, likes, id) {
+    if (e.target.innerHTML === 'Like') {
+      likes = likes+=1;
+      this.updateBeerLikes(id, likes);
+    } else {
+      likes = likes-=1;
+      this.updateBeerLikes(id, likes);
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Your Beer Cooler</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <h2>The Best Beer Cooler</h2>
         </div>
-        <BeerCards data={ this.state.beer }/>
+        <BeerCards data={ this.state.beer } onClick={ (e, likes, id) => this.updateLikes(e, likes, id) }/>
       </div>
     );
   }
-}
+};
 
 export default App;
--- src/BeerCards/BeerCards.js
+++ src/BeerCards/BeerCards.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './BeerCards.css';
 
-const BeerCards = ({ data }) => {
+const BeerCards = ({ data, onClick }) => {
   return (
     <div className="beer-card">
     {
       data.map(beer => {
         return (
-          <div key={ beer.id }>
-            <p>{ beer.name } likes: { beer.likes }</p>
-            <button>Like</button>
-            <button>Dislike</button>
+          <div key={ beer.id } data-id={ beer.id }>
+            <h3>{ beer.name }</h3>
+            <p>likes: <span className="beer-likes">{ beer.likes }</span></p>
+            <button onClick={ (e, likes, id) => onClick(e, beer.likes, beer.id) }>Like</button>
+            <button onClick={ (e, likes, id) => onClick(e, beer.likes, beer.id) }>Dislike</button>
           </div>
         )
       })
