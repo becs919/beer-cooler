554c1de33852d3215b2b75e54313bb53a1d8d3ad
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-06-21 17:53:04 -0600

    Add background photo, add some styling

--- src/BeerCards/BeerCards.js
+++ src/BeerCards/BeerCards.js
@@ -3,13 +3,13 @@ import './BeerCards.css';
 
 const BeerCards = ({ data, onClick }) => {
   return (
-    <div className="beer-card">
+    <div className='beer-card'>
     {
       data.map(beer => {
         return (
           <div key={ beer.id } data-id={ beer.id }>
             <h3>{ beer.name }</h3>
-            <p>likes: <span className="beer-likes">{ beer.likes }</span></p>
+            <p>likes: <span className='beer-likes'>{ beer.likes }</span></p>
             <button className='like-button' onClick={ (e, likes, id) => onClick(e, beer.likes, beer.id) }>Like</button>
             <button className='dislike-button' onClick={ (e, likes, id) => onClick(e, beer.likes, beer.id) }>Dislike</button>
           </div>
